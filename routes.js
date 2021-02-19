8688ae5c5546cf2daf5164bc84beff09feb02702
kkhh9469 <66230761+kkhh9469@users.noreply.github.com>
2021-02-20 01:31:21 +0900

    install wabpack and etc

--- routes.js
+++ routes.js
@@ -15,7 +15,7 @@ const CHANGE_PASSWORD = "/change-password";
 const VIDEO = "/videos";
 const UPLOAD = "/upload";
 const VIDEO_DETAIL = "/:id";
-const EDIT_VIDEO = "/:id/edit-video";
+const EDIT_VIDEO = "/:id/edit";
 const DELETE_VIDEO = "/:id/delete";
 
 const routes = {
@@ -43,8 +43,20 @@ const routes = {
       return VIDEO_DETAIL;
     }
   },
-  editVideo: EDIT_VIDEO,
-  deleteVideo: DELETE_VIDEO,
+  editVideo: (id) => {
+    if (id) {
+      return `/videos/${id}/edit`;
+    } else {
+      return EDIT_VIDEO;
+    }
+  },
+  deleteVideo: (id) => {
+    if (id) {
+      return `/videos/${id}/delete`;
+    } else {
+      return DELETE_VIDEO;
+    }
+  },
 };
 
 export default routes;
